4ac3664e1a9ce3cbbd6bb0b75cf5db2d03467006
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-28 11:02:03 +0330

    Data passed to the exporter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyfiglet import figlet_format
 from colorful import colorful as cf
 from scraper import Scraper
 from parser import Parser
+from exporter import Exporter
 
 
 class Main:
@@ -9,6 +10,7 @@ class Main:
         self.invalid_input = cf.red("invalid input!")
         self.scraper = Scraper()
         self.parser = Parser()
+        self.exporter = Exporter()
 
     def run(self):
         print("What do you looking for?")
@@ -27,8 +29,15 @@ class Main:
         except ValueError:
             print(self.invalid_input)
         else:
+            # Scraper get data from website
             data_iter = iter(self.scraper.get_data(search_title, page_number))
+
+            # Data that we have gotten, sent to parser to exctract data
             data = self.parser.extract_data(data_iter, stars)
+
+            # Save extracted data
+            self.exporter.save_data(data)
+
 
 
 if __name__ == "__main__":
