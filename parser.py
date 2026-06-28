c68e3167b4812de5a19b87e355d02d81a551023e
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-28 11:02:47 +0330

    In parser if english title not found, it use persian title instead

--- parser.py
+++ parser.py
@@ -3,16 +3,18 @@ from colorful import colorful as cf
 
 class Parser:
     def __init__(self) -> None:
-        print(cf.blue("[Info] Parser running."))
         self.data_dict = {}
     
     def extract_data(self, data, stars):
         for products in data:
             for i, product in enumerate(products.json()["data"]["products"]):
                 product_title = product["title_en"]
+                if not product_title:
+                    product_title = product["title_fa"]
+                    
                 product_stars = product["default_variant"]["seller"]["stars"]
                 product_price = product["default_variant"]["price"]["selling_price"]
-                product_url = product["url"]["uri"]
+                product_url = "https://www.digikala.com" + product["url"]["uri"]
 
                 if product_stars < stars:
                     continue
