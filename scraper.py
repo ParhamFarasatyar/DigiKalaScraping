94cc7a2888e085c612c96b8d3930e6469cbe50bc
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-28 11:03:06 +0330

    Debbuging print removed

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from colorful import colorful as cf
 class Scraper:
     def __init__(self) -> None:
         self.url = "https://api.digikala.com/discovery/api/v1/search"
-        print(cf.blue("[Info] Scraper running."))
     
     def get_data(self, title: str, page_number: int):
         for page in range(page_number):
