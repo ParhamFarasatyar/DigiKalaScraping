04a251c884218091f3a15a74754864808ed165a1
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-28 12:34:07 +0330

    Log file added for program messages and some modules add and removed

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from pyfiglet import figlet_format
-from colorful import colorful as cf
 from scraper import Scraper
 from parser import Parser
 from exporter import Exporter
+from log import Log
+from pyfiglet import figlet_format
 
 
 class Main:
     def __init__(self) -> None:
-        self.invalid_input = cf.red("invalid input!")
         self.scraper = Scraper()
         self.parser = Parser()
         self.exporter = Exporter()
+        self.log = Log()
 
-    def run(self):
+    def run(self) -> None:
+        welcome = figlet_format("DigiKala Scraping!")
+        print(self.log.run_msg(welcome))
         print("What do you looking for?")
         search_title = str(input(">>> "))
         print("How many pages you want to collect?")
@@ -20,29 +23,35 @@ class Main:
             while True:
                 page_number = int(input(">>> "))
                 if not 1 <= page_number <= self.scraper.max_pages:
-                    print(self.invalid_input)
+                    print(self.log.error_msg("Invalid input!"))
                 else:
                     break
             while True:
                 print("How many stars product got?")
                 stars = float(input(">>> "))
                 if not 1 <= stars <= 5:
-                    print(self.invalid_input)
+                    print(self.log.error_msg("Invalid input!"))
                     stars = float(input(">>> "))
                 else:
                     break
         except ValueError:
-            print(self.invalid_input)
+            print(self.log.error_msg("Invalid input!"))
             self.run()
         else:
             # Scraper get data from website
+            print(self.log.info_msg("Collecting data..."))
             data_iter = iter(self.scraper.get_data(search_title, page_number))
 
             # Data that we have gotten, sent to parser to exctract data
+            print(self.log.info_msg("Exctracting..."))
             data = self.parser.extract_data(data_iter, stars)
 
             # Save extracted data
+            print(self.log.info_msg("Saving in data folder..."))
             self.exporter.save_data(data)
+
+            print(self.log.bye_msg(
+                f"Data for {search_title.title()} successfully collected."))
 
 
 
