ff6a616ec81a7d74d322eebab012480d7d3c5ecb
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-29 15:12:53 +0330

    Error/Info message for API response created

--- log.py
+++ log.py
@@ -7,3 +7,11 @@ class Log:
         self.info_msg = cf.yellow
         self.run_msg = cf.cyan
         self.bye_msg = cf.green
+    
+    def checking_api_call(self, code_status):
+        if code_status == 200:
+            print(self.info_msg("API call was successful."))
+            return True
+        else:
+            print(self.error_msg("API call failed"))
+            return False
