d61a7581ff3290910fb97cd24a8e6b42f518d6ae
ParhamFarasatyar <pfarasatyar82@gmail.com>
2026-06-29 15:35:32 +0330

    Createed a method that checks user input for number of pages and stars

--- log.py
+++ log.py
@@ -15,3 +15,21 @@ class Log:
         else:
             print(self.error_msg("API call failed"))
             return False
+    
+    def checking_user_input(self, max_pages: int = 30) -> tuple[int, float]:
+        try:
+            while True:
+                print("How many pages you want to collect?(maximum limit is 30)")
+                page_number = int(input(">>> "))
+
+                print("How many stars got at least?")
+                stars = float(input(">>> "))
+
+                if (not 1 <= page_number <= max_pages) or (
+                    not 1 <= stars <= 5):
+                    print(self.error_msg("Invalid input!"))
+                else:
+                    return page_number, stars
+        except ValueError:
+            print(self.error_msg("Invalid input!"))
+            return 0, 0
